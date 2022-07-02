195f57779d5135b5a56571391e0484fb504ee964
Mürsel <murselcetin51@gmail.com>
2022-07-02 17:39:40 +0300

    Bana özel sayfası tasarlandı.

--- app/src/main/java/com/example/primevideo/BanaOzelFragment.kt
+++ app/src/main/java/com/example/primevideo/BanaOzelFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.primevideo.databinding.BanaozelRecyclerviewBinding
+import com.example.primevideo.databinding.FragmentBanaOzelBinding
 
 class BanaOzelFragment : Fragment() {
-
+    private lateinit var binding: FragmentBanaOzelBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bana_ozel, container, false)
+        binding = FragmentBanaOzelBinding.inflate(inflater, container, false)
+
+        binding.banaOzelRv.setHasFixedSize(true)
+        binding.banaOzelRv.layoutManager = LinearLayoutManager(this.context)
+
+
+        val filmlerListesi = ArrayList<Filmler>()
+        val f1 = Filmler(1, "The Terminal List", "terminallist")
+        val f2 = Filmler(2, "The Boyz", "theboys")
+        val f3 = Filmler(3, "Bang Bang Baby", "bangbang")
+        val f4 = Filmler(4, "Fight Club", "fightclub")
+        filmlerListesi.add(f1)
+        filmlerListesi.add(f2)
+        filmlerListesi.add(f3)
+        filmlerListesi.add(f4)
+
+        val BanaOzelAdapter = BanaOzelAdapter(requireContext(), filmlerListesi)
+        binding.banaOzelRv.adapter = BanaOzelAdapter
+
+        return binding.root
     }
 
 }
